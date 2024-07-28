4b3488b22d36e619e6f478da6dace2f27809eaff
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-07-28 14:50:30 +0530

    feat: add metatags

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -8,8 +8,23 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FeedItBack",
+  title: "Perception",
   description: "Collect feedbacks and share easily over social media",
+  openGraph: {
+    title: "Perception",
+    description: "Collect feedbacks and share easily over social media",
+    type: "website",
+    locale: "en_US",
+    url: "https://feedback.ashutoshrath.me",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dhnkuonev/image/upload/v1722158086/Screenshot_2024-07-28_144256_sbny2d.png",
+        width: 1200,
+        height: 630,
+        alt: "Perception",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +40,7 @@ export default function RootLayout({
             <div className="bg-transparent rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 p-4">
               <div className="container mx-auto flex justify-between items-center">
                 <Link href="/" className="text-xl font-bold">
-                  FeedItBack
+                  Perception
                 </Link>
 
                 <a
@@ -45,7 +60,7 @@ export default function RootLayout({
 
             <div className="bg-transparent text-black p-4">
               <div className="container mx-auto text-center flex flex-row items-center justify-between">
-                <p>&copy; 2024 FeedItBack. All rights reserved.</p>
+                <p>&copy; 2024 Perception. All rights reserved.</p>
                 <div className="">
                   <a
                     href="https://github.com/ashutosh-rath02"
